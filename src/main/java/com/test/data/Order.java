5e3d7b99317f1a985a173e2729eddf0ff9044e51
sindhu <sjasthi9@gmail.com>
2017-09-11 10:57:03 -0400

    Commiting the project

---
+++ src/main/java/com/test/data/Order.java
@@ -0,0 +1,38 @@
+package com.test.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.test.data.OrderLine;
+
+public class Order {
+	private List<OrderLine> orderLines = new ArrayList<>();
+
+	public void add(OrderLine o) throws Exception {
+		if (o == null) {
+			System.err.println("ERROR - Order is NULL");
+			throw new IllegalArgumentException("Order is NULL");
+		}
+		orderLines.add(o);
+	}
+
+	public int size() {
+		return orderLines.size();
+	}
+
+	public OrderLine get(int i) {
+		return orderLines.get(i);
+	}
+
+	public void clear() {
+		this.orderLines.clear();
+	}
+
+	public List<OrderLine> getOrderLines() {
+		return orderLines;
+	}
+
+	public void setOrderLines(List<OrderLine> orderLines) {
+		this.orderLines = orderLines;
+	}
+}
---
+++ src/main/java/com/test/service/Calculator.java
@@ -0,0 +1,71 @@
+package com.test.service;
+
+import java.util.Map;
+
+import com.test.data.Item;
+import com.test.data.Order;
+
+public class Calculator {
+	public static double rounding(double value) {
+		return (double) Math.round (value * 100.0)/ 100.0;
+	}
+
+	/**
+	 * receives a collection of orders. For each order, iterates on the order lines and calculate the total price which
+	 * is the item's price * quantity * taxes.
+	 * 
+	 * For each order, print the total Sales Tax paid and Total price without taxes for this order
+	 */
+	public void calculate(Map<String, Order> o) {
+
+		double grandtotal = 0;
+
+		// Iterate through the orders
+		for (Map.Entry<String, Order> entry : o.entrySet()) {
+			System.out.println("*******" + entry.getKey() + "*******");
+
+			Order r = entry.getValue();
+
+			double totalTax = 0;
+			double total = 0;
+
+			// Iterate through the items in the order
+			for (int i = 0; i < r.size(); i++) {
+
+				// Calculate the taxes
+				double tax = 0;
+
+				Item item = r.get(i).getItem();
+				float price = item.getPrice();
+				
+				if (item.getDescription().toLowerCase().contains("imported")) {
+					tax = price * 0.15; // Extra 5% tax on
+					// imported items
+				} else {
+					tax = price * 0.10;
+				}
+
+				// Calculate the total price
+				double totalprice = rounding(price + tax);
+
+				// Print out the item's total price
+				System.out.println(item.getDescription() + ": " + totalprice);
+
+				// Keep a running total
+				totalTax += tax;
+				total += price;
+			}
+
+			// Print out the total taxes
+			System.out.println("Sales Tax: " + rounding(totalTax));
+
+			total = rounding(total);
+
+			// Print out the total amount
+			System.out.println("Total: " + total);
+			grandtotal += total;
+		}
+
+		System.out.println("Sum of orders: " + rounding(grandtotal));
+	}
+}
